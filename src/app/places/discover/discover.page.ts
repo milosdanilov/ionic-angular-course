992ab94d91c3aebe15f3c66cb804761163c45d3d
Milos Danilov <milosdanilov@gmail.com>
2020-12-02 22:19:33 +0100

    139. Implementing Virtual Scrolling

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -10,11 +10,13 @@ import { PlacesService } from '../places.service';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
 }
